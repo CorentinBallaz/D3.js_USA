f7944d0bfd0a830c2d6b0394bfe1e73ae84bc639
CorentinBallaz <corentin.ballaz@yahoo.fr>
2020-01-13 21:29:41 +0100

    first commit

---
+++ bundle.js
@@ -0,0 +1,84 @@
+(function (d3) {
+  'use strict';
+
+  const xAxisLabelText = 'Nombre de victimes';
+
+  const svg = d3.select('svg');
+
+  const width = +svg.attr('width');
+  const height = +svg.attr('height');
+
+  const render = data => {
+    const xValue = d => d['victim'];
+    const yValue = d => d.year;
+    const margin = { top: 50, right: 40, bottom: 77, left: 180 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, xValue)])
+      .range([0, innerWidth]);
+    
+    const yScale = d3.scaleBand()
+      .domain(data.map(yValue))
+      .range([0, innerHeight])
+      .padding(0.1);
+
+    var div = d3.select("body")
+        .append("div")   
+        .attr("class", "tooltip")               
+        .style("opacity", 0);
+    
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+    
+    const xAxisTickFormat = number =>
+      d3.format('.3s')(number)
+        .replace('G', 'B');
+    
+    const xAxis = d3.axisBottom(xScale)
+      .tickFormat(xAxisTickFormat)
+      .tickSize(-innerHeight);
+    
+    g.append('g')
+      .call(d3.axisLeft(yScale))
+      .selectAll('.domain, .tick line')
+        .remove();
+    
+    const xAxisG = g.append('g').call(xAxis)
+      .attr('transform', `translate(0,${innerHeight})`);
+    
+    xAxisG.select('.domain').remove();
+    
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 65)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'black')
+        .text(xAxisLabelText);
+    
+    g.selectAll('rect').data(data)
+      .enter().append('rect')
+        .attr('y', d => yScale(yValue(d)))
+        .attr('width', d => xScale(xValue(d)))
+        .attr('height', yScale.bandwidth())
+        .on("mouseover", function(d) {      
+      div.transition()        
+           .duration(200)      
+           .style("opacity", .9);      
+           div.text(d.year + " : "+ d.victim + " victimes")
+           .style("left", (d3.event.pageX) + "px")     
+           .style("top", (d3.event.pageY) + "px");    
+  });
+    
+  };
+
+  d3.csv('dataStates.csv').then(data => {
+    data.forEach(d => {
+      d.victim = +d.victim;
+      console.log(d);
+    });
+    render(data);
+  });
+
+}(d3));
